9a0e360c03d15768d6e33c9a3c82e5ffe9a11d92
javi <framirez76@uabc.edu.mx>
2025-12-19 15:00:39 -0800

    Add bank emails dictionary

---
+++ constants/banks.py
@@ -0,0 +1,7 @@
+bank_emails = {
+    'hey_banco': ['noreply@hey.inc', 'alertas@hey.inc', 'noreply@heybanco.com', 'alertas@heybanco.com'],
+    'nubank': ['nu@nu.com.mx'],
+    'rappi': ['rappi.nreply@rappi.com', 'no-reply@mailing.rappicard.com.mx'],
+    'paypal': ['service@paypal.com.mx'],
+    'banorte': ['notificaciones@banorte.com']
+}
