85d7bf7b9dc51a0cd244d3e9900434847db6d452
javi <framirez76@uabc.edu.mx>
2026-01-08 22:14:17 -0800

    refactor: switch to absolute import

--- core/parsers/hey_banco.py
+++ core/parsers/hey_banco.py
@@ -21,7 +21,7 @@ from unidecode import unidecode
 from constants.banks import SupportedBanks
 from models.transaction import Transaction
 
-from .base_parser import BaseBankParser
+from core.parsers.base_parser import BaseBankParser
 
 logger = logging.getLogger("expense_tracker")
 
