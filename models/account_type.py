87801cb3f2283679ab41a7c700bff40049cca2e6
javi <framirez76@uabc.edu.mx>
2026-01-22 23:58:50 -0800

    style: fix whitespace in AccountType model definition

--- models/account_type.py
+++ models/account_type.py
@@ -2,10 +2,11 @@
 
 from sqlmodel import Field, SQLModel
 
+
 class AccountType(SQLModel, table=True):
     """Represents an account type"""
 
-    __tablename__ = "account_type" # type: ignore
+    __tablename__ = "account_type"  # type: ignore
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True, unique=True)
     description: str | None = Field(default=None)
