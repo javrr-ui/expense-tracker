8bb31d5482c5c44cbbd169caee1d56d5bf3575cd
javi <framirez76@uabc.edu.mx>
2026-01-26 23:48:15 -0800

    fix: add type ignore comment for SQLAlchemy table name in Category model

--- models/category.py
+++ models/category.py
@@ -6,7 +6,7 @@ from sqlmodel import Field, SQLModel
 class Category(SQLModel, table=True):
     """Represents a transaction category"""
 
-    __tablename__ = "category"
+    __tablename__ = "category" # type: ignore
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True, unique=True)
     description: str | None = None
